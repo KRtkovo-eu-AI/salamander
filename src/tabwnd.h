8638ee20fdb8d601c3cac0766314d848b47c6f66
Ondřej Kotas <ok@krtkovo.eu>
2025-09-26 21:05:59 +0200

    Always repaint tabs with system font

--- src/tabwnd.h
+++ src/tabwnd.h
@@ -79,7 +79,6 @@ private:
     void MoveTabColor(int from, int to);
     STabColor* GetTabColor(int index);
     const STabColor* GetTabColor(int index) const;
-    bool HasAnyCustomTabColors() const;
     bool TryResolveTabColor(int index, COLORREF& color) const;
     void PaintCustomTabs(HDC hdc, const RECT* clipRect) const;
     void DrawColoredTab(HDC hdc, const RECT& itemRect, const wchar_t* text, COLORREF baseColor,
