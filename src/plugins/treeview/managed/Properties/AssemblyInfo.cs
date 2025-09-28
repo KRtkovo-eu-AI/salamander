6be5fc550f2a5a6e3e538ff1d2c139df03260769
Ondřej Kotas <ok@krtkovo.eu>
2025-09-28 16:38:50 +0200

    Fix TreeView managed assembly metadata

---
+++ src/plugins/treeview/managed/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Salamander Tree View Browser")]
+[assembly: AssemblyDescription("WinForms browser host for the Salamander Tree View plugin.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Altap Salamander Team")]
+[assembly: AssemblyProduct("Altap Salamander")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("a769bc7d-62cc-4792-a06f-9de8c6909ae5")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
