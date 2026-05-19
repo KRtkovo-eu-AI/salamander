dd64420d0a5d31865a197e9d3ff46df0c97b4202
Ondřej Kotas <ok@krtkovo.eu>
2026-05-20 01:59:43 +0200

    Open VM Connect action on Enter/double-click in panel

--- src/plugins/hypervm/fs.cpp
+++ src/plugins/hypervm/fs.cpp
@@ -367,7 +367,26 @@ public:
     virtual BOOL WINAPI ChangeDriveMenuItemContextMenu(HWND parent, int panel, int x, int y, CPluginFSInterfaceAbstract* pluginFS, const char* pluginFSName, int pluginFSNameIndex, BOOL isDetachedFS, BOOL& refreshMenu, BOOL& closeMenu, int& postCmd, void*& postCmdParam)
     { (void)parent; (void)panel; (void)x; (void)y; (void)pluginFS; (void)pluginFSName; (void)pluginFSNameIndex; (void)isDetachedFS; (void)refreshMenu; (void)closeMenu; (void)postCmd; (void)postCmdParam; return FALSE; }
     virtual void WINAPI ExecuteChangeDrivePostCommand(int panel, int postCmd, void* postCmdParam) { (void)panel; (void)postCmd; (void)postCmdParam; }
-    virtual void WINAPI ExecuteOnFS(int panel, CPluginFSInterfaceAbstract* pluginFS, const char* pluginFSName, int pluginFSNameIndex, CFileData& file, int isDir) { (void)panel; (void)pluginFS; (void)pluginFSName; (void)pluginFSNameIndex; (void)file; (void)isDir; }
+    virtual void WINAPI ExecuteOnFS(int panel, CPluginFSInterfaceAbstract* pluginFS, const char* pluginFSName, int pluginFSNameIndex, CFileData& file, int isDir)
+    {
+        (void)panel;
+        (void)pluginFS;
+        (void)pluginFSName;
+        (void)pluginFSNameIndex;
+
+        if (isDir != 0 || file.Name == NULL || file.Name[0] == 0)
+            return;
+
+        std::string vm = EscapePsSingleQuoted(file.Name);
+        bool success = RunHiddenPowerShell("$ErrorActionPreference='Stop'; Import-Module Hyper-V -ErrorAction Stop; vmconnect.exe localhost '" + vm + "'");
+        if (!success)
+        {
+            SalamanderGeneral->SalMessageBox(SalamanderGeneral->GetMainWindowHWND(),
+                                             "Unable to open VM desktop session.",
+                                             "Hyper-V Machines",
+                                             MB_OK | MB_ICONERROR);
+        }
+    }
     virtual BOOL WINAPI DisconnectFS(HWND parent, BOOL isInPanel, int panel, CPluginFSInterfaceAbstract* pluginFS, const char* pluginFSName, int pluginFSNameIndex) { (void)isInPanel; (void)panel; (void)pluginFSName; (void)pluginFSNameIndex; SalamanderGeneral->CloseDetachedFS(parent, pluginFS); return TRUE; }
     virtual void WINAPI ConvertPathToInternal(const char* fsName, int fsNameIndex, char* fsUserPart) { (void)fsName; (void)fsNameIndex; (void)fsUserPart; }
     virtual void WINAPI ConvertPathToExternal(const char* fsName, int fsNameIndex, char* fsUserPart) { (void)fsName; (void)fsNameIndex; (void)fsUserPart; }
