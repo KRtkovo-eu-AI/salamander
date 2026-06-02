d8464c72701e8e6b5b038940a842cb35aaf51b14
Ondřej Kotas <ok@krtkovo.eu>
2026-06-02 20:39:01 +0200

    Fix tree view splitter dark mode color

--- src/fileswn2.cpp
+++ src/fileswn2.cpp
@@ -74,6 +74,22 @@ static LRESULT CALLBACK TreeViewSplitSubclassProc(HWND hwnd, UINT message, WPARA
 
     switch (message)
     {
+    case WM_ERASEBKGND:
+        return TRUE;
+
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
+        RECT r;
+        GetClientRect(hwnd, &r);
+
+        HBRUSH hBrush = DarkModeShouldUseDarkColors() ? DarkModeGetPanelFrameBrush() : GetSysColorBrush(COLOR_BTNFACE);
+        FillRect(hdc, &r, hBrush);
+        EndPaint(hwnd, &ps);
+        return 0;
+    }
+
     case WM_SETCURSOR:
         SetCursor(LoadCursor(NULL, IDC_SIZEWE));
         return TRUE;
@@ -203,6 +219,8 @@ void CFilesWindow::UpdateTreeViewColors()
     TreeView_SetBkColor(HTreeView, GetTreeViewBkColor());
     TreeView_SetLineColor(HTreeView, GetTreeViewTextColor());
     InvalidateRect(HTreeView, NULL, FALSE);
+    if (HTreeSplit != NULL)
+        InvalidateRect(HTreeSplit, NULL, TRUE);
 }
 
 void CFilesWindow::SetTreeViewWidth(int width)
