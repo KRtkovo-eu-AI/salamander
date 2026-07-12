45011929c1e34bc38322f942e5e368056b6630a1
Ondřej Kotas <ok@krtkovo.eu>
2026-07-12 04:44:37 +0200

    Define SAL_MAX_PATH locally for salmon

--- src/salmon/precomp.h
+++ src/salmon/precomp.h
@@ -25,7 +25,10 @@
 
 #include "..\salmoncl.h"
 #include "..\darkmode.h"
-#include "..\consts.h"
+
+#ifndef SAL_MAX_PATH
+#define SAL_MAX_PATH 32768
+#endif
 
 #include "salmon.h"
 #include "compress.h"
