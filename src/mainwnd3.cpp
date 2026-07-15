556013612827ca05f9132ace2e0a6c1de4db08ef
Ondřej Kotas <ok@krtkovo.eu>
2026-07-15 22:28:59 +0200

    Let GDI scaling own DPI transitions

--- src/mainwnd3.cpp
+++ src/mainwnd3.cpp
@@ -3961,73 +3961,35 @@ MENU_TEMPLATE_ITEM AddToSystemMenu[] =
 
     case WM_DPICHANGED:
     {
-        int dpi = HIWORD(wParam);
-        if (lParam != 0)
-        {
-            const RECT* suggestedRect = reinterpret_cast<const RECT*>(lParam);
-            SetWindowPos(HWindow, NULL, suggestedRect->left, suggestedRect->top,
-                         suggestedRect->right - suggestedRect->left,
-                         suggestedRect->bottom - suggestedRect->top,
-                         SWP_NOACTIVATE | SWP_NOZORDER);
-        }
-        PendingDPI = dpi;
-        PendingDPIWindowRectApplied = lParam != 0;
-        if (!DPIRefreshPosted)
-        {
-            DPIRefreshPosted = TRUE;
-            PostMessage(HWindow, WM_USER_APPLY_DPI_CHANGE, (WPARAM)dpi, 0);
-        }
-        return 0;
+        // In DPI_UNAWARE_GDISCALED mode Windows owns DPI transitions.
+        // Do not run Salamander's legacy per-monitor refresh path here; mixing
+        // both approaches causes double scaling and blurry/misaligned content.
+        break;
     }
 
     case WM_USER_APPLY_DPI_CHANGE:
     {
         DPIRefreshPosted = FALSE;
-        int dpi = PendingDPI > 0 ? PendingDPI : (int)wParam;
-        DPIWindowRectAlreadyApplied = PendingDPIWindowRectApplied;
         PendingDPI = 0;
         PendingDPIWindowRectApplied = FALSE;
-        RefreshDPI(TRUE, dpi, NULL);
         return 0;
     }
 
     case WM_DISPLAYCHANGE:
     {
         TraceDPIState("WM_DISPLAYCHANGE", HWindow);
-        if (!DPIRefreshPosted)
-        {
-            PendingDPI = GetDPIForWindow(HWindow);
-            DPIRefreshPosted = TRUE;
-            PostMessage(HWindow, WM_USER_APPLY_DPI_CHANGE, (WPARAM)PendingDPI, 0);
-        }
         break;
     }
 
     case WM_WTSSESSION_CHANGE:
     {
         TraceDPIState("WM_WTSSESSION_CHANGE", HWindow);
-        if (wParam == WTS_REMOTE_CONNECT || wParam == WTS_SESSION_LOGON ||
-            wParam == WTS_SESSION_UNLOCK)
-        {
-            if (!DPIRefreshPosted)
-            {
-                PendingDPI = GetCurrentSessionDPI();
-                DPIRefreshPosted = TRUE;
-                PostMessage(HWindow, WM_USER_APPLY_DPI_CHANGE, (WPARAM)PendingDPI, 0);
-            }
-        }
         return 0;
     }
 
     case WM_SETTINGCHANGE:
     {
         TraceDPIState("WM_SETTINGCHANGE", HWindow);
-        if (!DPIRefreshPosted)
-        {
-            PendingDPI = GetDPIForWindow(HWindow);
-            DPIRefreshPosted = TRUE;
-            PostMessage(HWindow, WM_USER_APPLY_DPI_CHANGE, (WPARAM)PendingDPI, 0);
-        }
         BOOL darkChanged = DarkModeHandleSettingChange(uMsg, lParam) ? TRUE : FALSE;
         if (darkChanged)
         {
