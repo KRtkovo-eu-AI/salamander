dd92dbda576b3f8dc19e9bdc4e7bfd07f854d585
Ondřej Kotas <ok@krtkovo.eu>
2026-07-21 21:01:08 +0200

    Fix fcremote ARM64 memcpy link

---
+++ src/plugins/filecomp/fcremote/fcremote_crt.cpp
@@ -0,0 +1,13 @@
+﻿// SPDX-FileCopyrightText: 2023 Open Salamander Authors
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include <stddef.h>
+
+extern "C" void* memcpy(void* dst, const void* src, size_t len)
+{
+    char* d = (char*)dst;
+    const char* s = (const char*)src;
+    while (len--)
+        *d++ = *s++;
+    return dst;
+}
