79e5d06d87b34ce6ed3f2bfb1e67af757a1e8eaa
Ondřej Kotas <ok@krtkovo.eu>
2026-07-26 19:12:44 +0200

    Fix Local Llama plugin build

--- src/plugins/salamatrixai/bundledprovider.cpp
+++ src/plugins/salamatrixai/bundledprovider.cpp
@@ -1,7 +1,9 @@
 // SPDX-FileCopyrightText: 2026 Open Salamander Authors
 // SPDX-License-Identifier: GPL-2.0-or-later
 
+#ifndef SALAMATRIXAI_BUNDLED_PROVIDER_HAS_PRECOMP
 #include "precomp.h"
+#endif
 #include "salamatrixai.h"
 #include <strsafe.h>
 
