4a4a42a402883370089c87ab42220868787e7eca
Ondřej Kotas <ok@krtkovo.eu>
2026-08-21 10:06:37 +0200

    include sfx

--- src/plugins/zip/vcxproj/zip2sfx/copytozip2sfxdir.bat
+++ src/plugins/zip/vcxproj/zip2sfx/copytozip2sfxdir.bat
@@ -1,26 +1,45 @@
 @echo off
 
-call :mycopy_with_mkdir_bat %2 %1 "%~1\..\..\..\..\Debug_x86\plugins\zip\zip2sfx" Debug_x86\plugins\zip\zip2sfx
-call :mycopy_with_mkdir_bat %2 %1 "%~1\..\..\..\..\Release_x64\plugins\zip\zip2sfx" Release_x64\plugins\zip\zip2sfx
-call :mycopy_with_mkdir_bat %2 %1 "%~1\..\..\..\..\Debug_x64\plugins\zip\zip2sfx" Debug_x64\plugins\zip\zip2sfx
+if /I "%~3"=="Debug" (
+  call :mycopy_with_mkdir_bat "%~2" "%~1" "%~1\..\..\..\..\Debug_x64\plugins\zip\zip2sfx" "Debug_x64\plugins\zip\zip2sfx"
+  if errorlevel 1 exit /b 1
+  exit /b 0
+)
 
-exit /b
+if /I "%~3"=="Release" (
+  call :mycopy_with_mkdir_bat "%~2" "%~1" "%~1\..\..\..\..\Release_x64\plugins\zip\zip2sfx" "Release_x64\plugins\zip\zip2sfx"
+  if errorlevel 1 exit /b 1
+  call :mycopy_with_mkdir_bat "%~2" "%~1" "%~1\..\..\..\..\Release_arm64\plugins\zip\zip2sfx" "Release_arm64\plugins\zip\zip2sfx"
+  if errorlevel 1 exit /b 1
+  exit /b 0
+)
+
+if /I "%~3"=="Release clean" (
+  call :mycopy_with_mkdir_bat "%~2" "%~1" "%~1\..\..\..\..\Release clean_x64\plugins\zip\zip2sfx" "Release clean_x64\plugins\zip\zip2sfx"
+  if errorlevel 1 exit /b 1
+  call :mycopy_with_mkdir_bat "%~2" "%~1" "%~1\..\..\..\..\Release clean_arm64\plugins\zip\zip2sfx" "Release clean_arm64\plugins\zip\zip2sfx"
+  if errorlevel 1 exit /b 1
+  exit /b 0
+)
+
+echo Unsupported ZIP2SFX configuration: "%~3"
+exit /b 1
 
 
 :mycopy_with_mkdir_bat
 
-echo Copying %~1 and %~n1.pdb to %4...
+echo Copying %~1 and %~n1.pdb to %~4...
 
-if not exist %3 mkdir %3
+if not exist "%~3" mkdir "%~3"
 
 copy "%~2\%~1" "%~3\"
 if errorlevel 1 (
   echo Error: unable to copy file "%~2\%~1" to "%~3".
-  exit 1
+  exit /b 1
 )
 copy "%~2\%~n1.pdb" "%~3\"
 if errorlevel 1 (
   echo Error: unable to copy file "%~2\%~n1.pdb" to "%~3\".
-  exit 1
+  exit /b 1
 )
 exit /b
